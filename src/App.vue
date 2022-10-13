e90ba13f5e46fecb2f551b623769a6aef11ddcc1
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-10-13 18:02:09 +0630

    refAndComponents

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 ref="greet">hi {{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="clickHandler">Click</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      title:'U dont know me'
+    }
+  },
+  methods:{
+    clickHandler()
+    {
+      this.$refs.name.focus()
+      this.$refs.greet.textContent = 'hi there'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1{
+  color:blue;
 }
 </style>
